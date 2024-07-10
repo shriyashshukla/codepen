2454e6aa51f6e7b276f5029c4534d73438f107c3
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-07-10 13:04:18 +0530

    chore: Update editor styles and layout

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+import '../styles/index.css';
+import App from '../components/App';
+
+export default function Home() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
